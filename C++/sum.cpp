8716b067b84cc80d0d45a744d3448ec9f0bc69bc
Ganesh Ravindra Kuppasta <155829643+ganesh94jrl@users.noreply.github.com>
2024-09-25 22:43:03 +0530

    Update sum.cpp

--- C++/sum.cpp
+++ C++/sum.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int main (){
-	int a, b;
+  int a, b;
+  cout<<"enter any two integer:"<<endl;
   cin>>a>>b;
   cout<<"The sum of the two number is "<<a+b;
   return 0;
