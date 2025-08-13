89ee5cb7df0a41b987be5cb27cf870f9364fb5f8
Ganesh Ravindra Kuppasta <155829643+ganesh94jrl@users.noreply.github.com>
2025-08-13 20:34:12 +0530

    Update calculator.py

--- Python/calculator.py
+++ Python/calculator.py
@@ -11,17 +11,21 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+def remainder(x,y):
+    return x % y
+
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Remainder")
 
 while True:
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '5'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -36,6 +40,9 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+
+        elif choice == '5':
+            print(num1, "%", num2, "=", remainder(num1, num2))
         break
     else:
         print("Invalid Input")
